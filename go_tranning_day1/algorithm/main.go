013d9f3841ff7dc3f9faf12906a98096aa4043d5
hieunc3 <hieunc3@rikkeisoft.com>
2022-05-04 15:59:14 +0700

    Rank sort with goroutines

--- go_tranning_day1/algorithm/main.go
+++ go_tranning_day1/algorithm/main.go
@@ -1,44 +1,103 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	numbers := []int{1, 4, 3, 2, 5, 7, 10, 3, 7, 8}
+	numbers := []int{37, 12, 28, 9, 100, 56, 80, 5, 12}
+	c := make(chan []int)
 	//Rank Sort (Depend on sorted slice)
-	fmt.Printf("Slice rank: %v \n", rankWithSortedSlice(numbers))
+	//fmt.Printf("Slice rank: %v \n", rankWithSortedSlice(numbers))
+
 	//Rank Sort
-	fmt.Printf("Slice rank: %v \n", rankSortWithOrginSlice(numbers))
+	// fmt.Println(strings.Repeat("=", 60))
+	// go rankSortWithOrginSlice(numbers, c)
+	// rankList := <-c
+	// printSlice(rankList)
+
+	go rankSortWithMap(numbers, c)
+	rankList := <-c
+	printSlice(rankList)
 }
 
-func rankWithSortedSlice(nums []int) []int {
-	var temp int
-	sliceRank := make([]int, len(nums))
-	//Sort a copy numbers slice
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] >= nums[j] {
-				temp = nums[i]
-				nums[i] = nums[j]
-				nums[j] = temp
+// func rankWithSortedSlice(nums []int) []int {
+// 	var temp int
+// 	sliceRank := make([]int, len(nums))
+// 	//Sort a copy numbers slice
+// 	for i := 0; i < len(nums)-1; i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i] >= nums[j] {
+// 				temp = nums[i]
+// 				nums[i] = nums[j]
+// 				nums[j] = temp
+// 			}
+// 		}
+// 	}
+// 	fmt.Printf("Slice after sort: %v \n", nums)
+// 	//Create default rank for the slice
+// 	for i := 0; i < len(nums); i++ {
+// 		sliceRank[i] = i + 1
+// 	}
+
+// 	//Update the rank for the slice
+// 	for j := 0; j < len(nums)-1; j++ {
+// 		if nums[j] == nums[j+1] {
+// 			sliceRank[j+1] = j + 1
+// 		}
+// 	}
+
+// 	return sliceRank
+// }
+
+// func rankSortWithOrginSlice(nums []int, rank_list chan []int) {
+// 	numsCopy := make([]int, len(nums))
+// 	rankArr := make([]int, len(nums))
+// 	copy(numsCopy, nums)
+// 	sort.Ints(numsCopy)
+
+// 	for i := 0; i < len(nums); i++ {
+// 		var count int = 1
+// 		for j := 0; j < len(nums); j++ {
+// 			if nums[i] > numsCopy[j] {
+// 				count++
+// 			}
+// 		}
+// 		rankArr[i] = count
+// 	}
+//  printSlice(nums)
+// 	rank_list <- rankArr
+// }
+
+func rankSortWithMap(nums []int, rank_list chan []int) {
+	defer close(rank_list)
+	arrMap := make(map[int]int)
+	sortedArr := make([]int, len(nums))
+	arrRank := make([]int, len(nums))
+	copy(sortedArr, nums)
+	sort.Ints(sortedArr)
+	rank := 1
+	for _, s := range sortedArr {
+		if _, ok := arrMap[s]; !ok {
+			arrMap[s] = rank
+			rank++
+		}
+	}
+	for i, v := range nums {
+		for key, element := range arrMap {
+			if v == key {
+				arrRank[i] = element
 			}
 		}
 	}
-	fmt.Printf("Slice after sort: %v \n", nums)
-	//Create default rank for the slice
-	for i := 0; i < len(nums); i++ {
-		sliceRank[i] = i + 1
-	}
-
-	//Update the rank for the slice
-	for j := 0; j < len(nums)-1; j++ {
-		if nums[j] == nums[j+1] {
-			sliceRank[j+1] = j + 1
-		}
-	}
-
-	return sliceRank
+	printSlice(nums)
+	rank_list <- arrRank
 }
 
-func rankSortWithOrginSlice(nums []int) []int {
-
+func printSlice(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%6d", arr[i])
+	}
+	fmt.Println()
 }
